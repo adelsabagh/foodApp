6bf37dc80c52922d67d9aadc6ae9f6423159ffad
Adel <44180457+adelsabagh@users.noreply.github.com>
2021-12-01 15:49:18 +0300

    Update ItemController.php

--- app/Http/Controllers/API/V1/ItemController.php
+++ app/Http/Controllers/API/V1/ItemController.php
@@ -11,7 +11,7 @@ class ItemController extends Controller
 
     public function index()
     {
-        return response()->json(Item::class);
+        return response()->json(Item::all());
     }
 
 
