a1b891826e60ee51df38bb9639dd919a9e30a523
adelsabagh <adolsabagh1996@gmail.com>
2021-12-04 11:56:50 +0300

    register changed

--- app/Http/Controllers/API/V1/UserController.php
+++ app/Http/Controllers/API/V1/UserController.php
@@ -34,7 +34,7 @@ class UserController extends Controller
 
     public function register(Request $request)
     {
-        return User::create([
+        $user =  User::create([
            'name'=>$request->name,
            'email'=>$request->email,
            'password'=>Hash::make($request->password),
